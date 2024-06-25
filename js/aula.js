1494650ae532561d9f0e253da1c51831a136b3a9
carlocoradini <carlo.schirmer.110@ufrn.edu.br>
2024-06-25 18:35:47 -0300

    Header, footer, e responsividade atualizada

--- js/aula.js
+++ js/aula.js
@@ -1,26 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const videoPlayer = document.getElementById("videoPlayer");
-    const videoSource = document.getElementById("videoSource");
-    const videoListItems = document.querySelectorAll(".video-list li");
     const moduleHeaders = document.querySelectorAll(".module h2");
     const questionForm = document.getElementById("question-form");
     const questionsList = document.getElementById("questions-list");
 
-    videoListItems.forEach(function(item) {
-        item.addEventListener("click", function() {
-            const videoSrc = this.getAttribute("data-video-src");
-            videoSource.src = videoSrc;
-            videoPlayer.load();
-            videoPlayer.play();
-
-            videoListItems.forEach(function(item) {
-                item.querySelector('.watched')?.classList.remove('watched');
-                item.querySelector('.not-watched')?.classList.add('not-watched');
-            });
-            this.querySelector('.not-watched').classList.remove('not-watched');
-            this.querySelector('.watched').classList.add('watched');
-        });
-    });
 
     moduleHeaders.forEach(function(header) {
         header.addEventListener("click", function() {
